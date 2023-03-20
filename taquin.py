36e95588ae4104782f0e04ef08a50bb101bdae26
oktaybagdatli <bagdatlioktay@gmail.com>
2023-03-20 19:34:34 +0100

    initialisation

---
+++ taquin.py
@@ -0,0 +1,17 @@
+global espace 
+
+espace =  0
+indexInitial = (2,1)
+etat_initial = [
+    [1,3,4],
+    [8,6,2],
+    [7,espace,5]
+]
+def succeseurs(etat):
+        succeseurs = []
+        for move in [-1,1]:
+            indexX = indexInitial[0]-1
+            
+                
+                 
+        return succeseurs
