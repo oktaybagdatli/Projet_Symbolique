8fca206d53b2c0a3414746e48f5d93126473a776
oktaybagdatli <bagdatlioktay@gmail.com>
2023-03-20 19:35:20 +0100

    inti

---
+++ Projet_Symbolique-1/modeles.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+
+        
+@dataclass
+class Chiffre():
+    color:str
+    valeur:int
+    def __post_init__(self,color,valeur):
+        self.color =color
+        self.valeur = valeur
+    
+@dataclass
+class Tige():
+    chiffres:list[Chiffre]
+    capacite:int = 3
+    
+    def ajouterChiffre(self,chiffre):
+        if self.capacite != 0:
+            self.chiffres.append(chiffre)
+    
+
+        
+@dataclass 
+class Etat():
+    tiges:list[Tige]
+    
+    
+    
+    def __post_init__(self,tiges):
+        for tige in tiges:
+            self.tiges.append(tige)
+            
+            
+            
+    def getTige(self,numeroTige):
+        return self.tiges[numeroTige-1]
+    
+    
+    def enleverRemettreChiffre(self,numDepart,numDestination):
+        tigeDep = self.getTige(numDepart)
+        tigeDest = self.getTige(numDestination)
+        chiffre = tigeDep.pop()
+        tigeDest.ajouterChiffre(chiffre)
+        
+            
+    
+    
+    
+    
+    
+    
